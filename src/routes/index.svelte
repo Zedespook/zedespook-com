0567ed1d52f929903f12d318be593c3f4f38f16b
Zedespook <26501287+Zedespook@users.noreply.github.com>
2022-07-06 14:08:35 +0200

    Removed SvelteKit example & added homepage base

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,45 @@
-<script context="module">
-  export const prerender = true;
-</script>
-
-<script>
-  import Counter from '$lib/Counter.svelte';
-</script>
-
-<svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-  <h1>
-    <div class="welcome">
-      <picture>
-        <source srcset="svelte-welcome.webp" type="image/webp" />
-        <img src="svelte-welcome.png" alt="Welcome" />
-      </picture>
+<body>
+  <section class="p-8">
+    <div class="alert alert-info shadow-lg">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="stroke-current flex-shrink-0 w-6 h-6"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          /></svg
+        >
+        <span>The website is under construction!</span>
+      </div>
     </div>
-
-    to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/index.svelte</strong>
-  </h2>
-
-  <Counter />
-</section>
-
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style>
+  </section>
+  <main class="container mx-auto p-4 lg:px-96">
+    <section>
+      <h1 class="text-2xl text-center p-4">Zedespook's Website</h1>
+      <p class="text-base lg:text-center p-4 bg-primary rounded-xl">
+        Welcome to Zedespook's website! You can find his video editing and coding projects here!
+      </p>
+    </section>
+    <section class="pt-8">
+      <div class="p-4">
+        <h1 class="text-lg underline">About</h1>
+        <p class="p-2 lg:px-16 lg:text-justify">
+          Zedespook has been a full-stack freelance developer from Hungary, but now based in
+          Denmark.
+        </p>
+        <p class="p-2 lg:px-16 lg:text-justify">
+          While being a developer professionally, he also enjoys doing his project to extend his
+          expertise.
+        </p>
+        <p class="p-2 lg:px-16 lg:text-justify">
+          Outside of programming, he edits his or his friend's videos for YouTube content.
+        </p>
+      </div>
+    </section>
+  </main>
+</body>
